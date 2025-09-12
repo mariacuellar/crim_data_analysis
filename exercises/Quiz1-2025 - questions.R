5581bd024aa940165186b62a18edc5a50d8d3a07
Maria Cuellar <maria.cuellar@gmail.com>
2025-09-12 16:18:26 -0400

    a

---
+++ exercises/Quiz1-2025 - questions.R
@@ -0,0 +1,22 @@
+# Quiz 1, 2025
+
+# Your name: 
+
+
+# 1. Load the data called domestic_violence.csv (5 points)
+
+
+# 2. What is this data about? (2 points)
+
+
+# 3. What are the variables in this dataset? (2 points)
+
+
+# 4. How many observations and variables are there in the dataset? (2 points)
+
+
+# 5. What type of statistical variable is Education? (2 points)
+
+
+# 6. What is the Marital status of the individuals who are older than 55 years old? (2 points)
+
